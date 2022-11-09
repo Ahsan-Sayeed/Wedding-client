5bc20a99301af6cead2f561d12ba903e287474f3
ahsan sayeed <ahsansayeedsaad@gmail.com>
2022-11-09 20:03:00 +0000

    blog page completed

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React from "react";
-import useTitle from '../../Hooks/useTitle';
+import useTitle from "../../Hooks/useTitle";
 
 const Blog = () => {
   useTitle("Blog");
@@ -13,20 +13,23 @@ const Blog = () => {
             className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900"
           >
             <img
-              src="https://source.unsplash.com/random/480x360"
+              src="https://miro.medium.com/max/1400/1*Z5SpsmDvk67BIImwHvh_cQ.png"
               alt=""
               className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500"
             />
             <div className="p-6 space-y-2 lg:col-span-5">
               <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
-                Noster tincidunt reprimique ad pro
+                Difference between SQL and NoSQL?
               </h3>
               <span className="text-xs dark:text-gray-400">
-                February 19, 2021
+                {new Date().toDateString()}
               </span>
               <p>
-                Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est
-                in graece fuisset, eos affert putent doctus id.
+                SQL is the programming language used to interface with
+                relational databases. (Relational databases model data as
+                records in rows and tables with logical links between them).
+                NoSQL is a class of DBMs that are non-relational and generally
+                do not use SQL.
               </p>
             </div>
           </a>
@@ -39,20 +42,23 @@ const Blog = () => {
               <img
                 role="presentation"
                 className="object-cover w-full rounded h-44 dark:bg-gray-500"
-                src="https://source.unsplash.com/random/480x360?1"
+                src="https://supertokens.com/static/b0172cabbcd583dd4ed222bdb83fc51a/9af93/jwt-structure.png"
               />
               <div className="p-6 space-y-2">
                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-                  In usu laoreet repudiare legendos
+                  What is JWT?
                 </h3>
                 <span className="text-xs dark:text-gray-400">
-                  January 21, 2021
+                  {new Date().toDateString()}
                 </span>
                 <p>
-                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                  neglegentur, ex has tantas percipit perfecto. At per tempor
-                  albucius perfecto, ei probatus consulatu patrioque mea, ei
-                  vocent delicata indoctum pri.
+                  JSON Web Token (JWT) is an open standard (RFC 7519) that
+                  defines a compact and self-contained way for securely
+                  transmitting information between parties as a JSON object.
+                  This information can be verified and trusted because it is
+                  digitally signed. JWTs can be signed using a secret (with the
+                  HMAC algorithm) or a public/private key pair using RSA or
+                  ECDSA
                 </p>
               </div>
             </a>
@@ -64,20 +70,20 @@ const Blog = () => {
               <img
                 role="presentation"
                 className="object-cover w-full rounded h-44 dark:bg-gray-500"
-                src="https://source.unsplash.com/random/480x360?2"
+                src="https://www.partech.nl/-/media/habitat/images/blog/authentication-using-json-web-tokens-in-net-core/how-does-jwt-work.ashx?mw=900&hash=F97576EA7092091B6A30C44DC5681185"
               />
               <div className="p-6 space-y-2">
                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-                  In usu laoreet repudiare legendos
+                  How does JWT work?
                 </h3>
                 <span className="text-xs dark:text-gray-400">
-                  January 22, 2021
+                  {new Date().toDateString()}
                 </span>
                 <p>
-                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                  neglegentur, ex has tantas percipit perfecto. At per tempor
-                  albucius perfecto, ei probatus consulatu patrioque mea, ei
-                  vocent delicata indoctum pri.
+                  Basically the identity provider(IdP) generates a JWT
+                  certifying user identity and Resource server decodes and
+                  verifies the authenticity of the token using secret salt /
+                  public key
                 </p>
               </div>
             </a>
@@ -89,14 +95,65 @@ const Blog = () => {
               <img
                 role="presentation"
                 className="object-cover w-full rounded h-44 dark:bg-gray-500"
-                src="https://source.unsplash.com/random/480x360?3"
+                src="https://kinsta.com/wp-content/uploads/2021/03/Nodejs-Architecture.png"
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  What is the difference between javascript and Node JS?
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  {new Date().toDateString()}
+                </span>
+                <p>
+                  JavaScript is a simple programming language that can be used
+                  with any browser that has the JavaScript Engine installed.
+                  Node. js, on the other hand, is an interpreter or execution
+                  environment for the JavaScript programming language
+                </p>
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block"
+            >
+              <img
+                role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://forum.golibrary.co/wp-content/uploads/2020/04/nodejs.png"
+              />
+              <div className="p-6 space-y-2">
+                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                  How does NodeJS handle multiple requests at the same time?
+                </h3>
+                <span className="text-xs dark:text-gray-400">
+                  {new Date().toDateString()}
+                </span>
+                <p>
+                  How NodeJS handle multiple client requests? NodeJS receives
+                  multiple client requests and places them into EventQueue.
+                  NodeJS is built with the concept of event-driven architecture.
+                  NodeJS has its own EventLoop which is an infinite loop that
+                  receives requests and processes them.
+                </p>
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block"
+            >
+              <img
+                role="presentation"
+                className="object-cover w-full rounded h-44 dark:bg-gray-500"
+                src="https://www.partech.nl/-/media/habitat/images/blog/authentication-using-json-web-tokens-in-net-core/how-does-jwt-work.ashx?mw=900&hash=F97576EA7092091B6A30C44DC5681185"
               />
               <div className="p-6 space-y-2">
                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
                   In usu laoreet repudiare legendos
                 </h3>
                 <span className="text-xs dark:text-gray-400">
-                  January 23, 2021
+                  {new Date().toDateString()}
                 </span>
                 <p>
                   Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
@@ -114,64 +171,14 @@ const Blog = () => {
               <img
                 role="presentation"
                 className="object-cover w-full rounded h-44 dark:bg-gray-500"
-                src="https://source.unsplash.com/random/480x360?4"
+                src="https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/687489/retina_800x320_cover-0801_SONWebTokenTutorialAnExampleinLaravelandAngularJS_Razvan_Newsletter-a059e94497ffd6d077fee09ef992526b.png"
               />
               <div className="p-6 space-y-2">
                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
                   In usu laoreet repudiare legendos
                 </h3>
                 <span className="text-xs dark:text-gray-400">
-                  January 24, 2021
-                </span>
-                <p>
-                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                  neglegentur, ex has tantas percipit perfecto. At per tempor
-                  albucius perfecto, ei probatus consulatu patrioque mea, ei
-                  vocent delicata indoctum pri.
-                </p>
-              </div>
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block"
-            >
-              <img
-                role="presentation"
-                className="object-cover w-full rounded h-44 dark:bg-gray-500"
-                src="https://source.unsplash.com/random/480x360?5"
-              />
-              <div className="p-6 space-y-2">
-                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-                  In usu laoreet repudiare legendos
-                </h3>
-                <span className="text-xs dark:text-gray-400">
-                  January 25, 2021
-                </span>
-                <p>
-                  Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
-                  neglegentur, ex has tantas percipit perfecto. At per tempor
-                  albucius perfecto, ei probatus consulatu patrioque mea, ei
-                  vocent delicata indoctum pri.
-                </p>
-              </div>
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900 hidden sm:block"
-            >
-              <img
-                role="presentation"
-                className="object-cover w-full rounded h-44 dark:bg-gray-500"
-                src="https://source.unsplash.com/random/480x360?6"
-              />
-              <div className="p-6 space-y-2">
-                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-                  In usu laoreet repudiare legendos
-                </h3>
-                <span className="text-xs dark:text-gray-400">
-                  January 26, 2021
+                  {new Date().toDateString()}
                 </span>
                 <p>
                   Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
